9f938400ab527568fa7ad6b2b3cb3de3faacab9b
roman.tihonov <roman.tihonov@a0d4120e-ab21-11dd-8fd9-859366ca643a>
2008-11-10 12:19:47 +0000

    commit iz univera

---
+++ RVGBilling/RVGbilling/Model/CallStrategy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace RVGbilling.Model
+{
+    public class CallStrategy : IDBStrategy
+    {
+        private SqlConnection _Connection;
+        string query = "select * from calls where ";
+
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
+        public CallStrategy()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+
+        public void setConnection(SqlConnection conn) { }
+
+        public List<DBAbstractObject> getFromDB(string query) { return null; }
+
+        public void AddItemToDB(DBAbstractObject item) { }
+
+        public void UpdateItemToDB(DBAbstractObject item) { }
+
+        public void RemoveItemFromDB(DBAbstractObject item) { }
+    }
+}
