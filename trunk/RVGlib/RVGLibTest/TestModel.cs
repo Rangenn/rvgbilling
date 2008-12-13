39b52c65371bce18f336e1d478c1f400c36d1409
roman.tihonov <roman.tihonov@a0d4120e-ab21-11dd-8fd9-859366ca643a>
2008-12-13 16:01:32 +0000

    nhibernate, read changes.xls!

---
+++ trunk/RVGlib/RVGLibTest/TestModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RVGLibTest
+{
+
+    public class TestModel : FluentNHibernate.PersistenceModel
+    {
+        public TestModel()
+        {
+            addMappingsFromAssembly(typeof(RVGlib.Mappings.AbonentMap).Assembly);
+           
+        }      
+    }
+}
---
+++ trunk/RVGlib/RVGlib/Mappings/RateMap.cs
@@ -0,0 +1,15 @@
+﻿using RVGlib.Domain;
+using FluentNHibernate.Mapping;
+
+namespace RVGlib.Mappings
+{
+    public class RateMap : ClassMap<Rate>
+    {
+        public RateMap()
+        {
+            this.TableName = "rates";
+            Id(x => x.Id);
+            Map(x => x.name).CanNotBeNull().WithLengthOf(100);
+        }
+    }
+}
