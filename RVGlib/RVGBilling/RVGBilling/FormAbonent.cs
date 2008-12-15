1e8841a5b7133c7abb1cbdb2675e3ec4083294a2
spokoistvie <spokoistvie@a0d4120e-ab21-11dd-8fd9-859366ca643a>
2008-12-15 19:08:49 +0000

    основной проект чистый - одни формочки

---
+++ RVGlib/RVGBilling/RVGBilling/FormAbonent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RVGBilling
+{
+    public partial class FormAbonent : Form
+    {
+        Controller ctrl;
+
+        public FormAbonent(Controller ctrl)
+        {
+            InitializeComponent();
+            this.ctrl = ctrl;
+        }
+
+        public FormAbonent(Controller ctrl, string FIO, string passport):this(ctrl)
+        {
+            tbName.Text = FIO;
+            tbPassport.Text = passport;
+        }
+    }
+}
