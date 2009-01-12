9c8167b3e7f6b06a569a82d04da7e78c76fe6535
vkotovv <vkotovv@a0d4120e-ab21-11dd-8fd9-859366ca643a>
2009-01-12 12:53:01 +0000

    Parser and Importer added

---
+++ RVGlib/RVGlib/Import/CSVParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RVGlib.Import
+{
+    public class CSVParser
+    {
+        private static string separator = ",";
+        private static string pattern= string.Format("{0}(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))",separator);
+
+        public static string[] Split(string input)
+        {
+            return Regex.Split(input, pattern);
+        }
+    }
+}
